f9768a9e187df215cd1850b9d088e4554bd633d2
Woohyun Park <woohyunpark.mail@gmail.com>
2022-01-06 18:05:34 +0900

    PRACTICE: practice redux more

--- src/index.js
+++ src/index.js
@@ -7,8 +7,9 @@ import reportWebVitals from "./reportWebVitals";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "./modules";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
   <React.StrictMode>
